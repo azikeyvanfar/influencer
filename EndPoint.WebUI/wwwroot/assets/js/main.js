0b18342f098e324837b42face41289a7c33bccdd
azadeh <azadeh@DESKTOP-PJRLJUC>
2021-08-09 10:31:17 +0430

    mirror

--- EndPoint.WebUI/wwwroot/assets/js/main.js
+++ EndPoint.WebUI/wwwroot/assets/js/main.js
@@ -283,9 +283,9 @@
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
+      easing: 'ease-in-out'//,
       //once: true,
-      mirror: false
+      //mirror: false
     })
   });
 
